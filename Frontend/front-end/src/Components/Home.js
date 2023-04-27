cb2f8b3a622851227289d9e6c24c4fe67fe4c415
Junaid Ansari <ansarijunaid957@gmail.com>
2023-04-27 17:52:20 +0530

    Loogin featur successfully implement

--- Frontend/front-end/src/Components/Home.js
+++ Frontend/front-end/src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Home() {
+  console.log(localStorage)
+
   return (
     <div>
       Home
--- Frontend/front-end/src/Components/Login.js
+++ Frontend/front-end/src/Components/Login.js
@@ -34,47 +34,63 @@ export default function Login() {
       username: username,
       password: password
     }
-    console.log(user)
+    // console.log(user)
 
     // Create the POST request
-    const resp = await axios.post('http://64.227.146.222/token/', user)
-    console.log(resp.data)
+    try {
+      const resp = await axios.post('http://64.227.146.222/token/', user)
+      let tokenObj = resp.data
+      // console.log(tokenObj)
+
+      // initialize the access and refresh token in localstorage 
+      localStorage.clear();
+      localStorage.setItem('access_token', tokenObj.access);
+      localStorage.setItem('refresh_token', tokenObj.refresh);
+      // console.log(localStorage)
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${tokenObj['access']}`;
+      window.location.href = '/'
+    }
+    catch (e) {
+      alert(e)
+
+    }
 
 
 
-}
+  }
 
 
-const MyStyle = {
-  borderRadius: '15px',
-  boxShadow: '5px 12px 8px gray'
-}
-return (
-  <div className='container my-5 '>
-    <div style={MyStyle} className='container my-5 p-5 bg-light w-50' >
+  const MyStyle = {
+    borderRadius: '15px',
+    boxShadow: '5px 12px 8px gray'
+  }
+  return (
+    <div className='container my-5 '>
+      <div style={MyStyle} className='container my-5 p-5 bg-light w-50' >
 
-      <form onSubmit={submit}>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" className="form-control" id="username" required onChange={event => setUsername(event.target.value)} />
+        <form onSubmit={submit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" className="form-control" id="username" required onChange={event => setUsername(event.target.value)} />
 
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" required onChange={evenet => setPassword(evenet.target.value)} />
-        </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" required onChange={evenet => setPassword(evenet.target.value)} />
+          </div>
 
-        <div className="form-group form-check">
-          <input type="checkbox" className="form-check-input" id="showpass" onClick={ShowPass} />
-          <label className="form-check-label" htmlFor="showpass">Show password</label>
-        </div>
-        <button type="submit" className="btn btn-primary" >Login</button>
-      </form>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="showpass" onClick={ShowPass} />
+            <label className="form-check-label" htmlFor="showpass">Show password</label>
+          </div>
+          <button type="submit" className="btn btn-primary" >Login</button>
+        </form>
 
-    </div >
+      </div >
 
 
 
-  </div>
-)
+    </div>
+  )
 }
