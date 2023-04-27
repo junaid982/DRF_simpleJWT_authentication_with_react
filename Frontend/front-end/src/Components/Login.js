0c9543169d9b2fa13f59a287aae7cb8e28e94b5d
Junaid Ansari <ansarijunaid957@gmail.com>
2023-04-27 17:27:22 +0530

    created Login Component fetching data from input perfectly

--- Frontend/front-end/src/Components/Login.js
+++ Frontend/front-end/src/Components/Login.js
@@ -1,9 +1,76 @@
 import React from 'react'
 
+import { useState } from 'react';
+
+
 export default function Login() {
-  return (
-    <div>
-      Login
-    </div>
-  )
+
+  // set default username password value 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Show Password Logic CheckBox 
+
+  const ShowPass = () => {
+    const pass = document.getElementById('password');
+    if (pass.type === 'password') {
+      pass.type = 'text'
+    }
+    else {
+      pass.type = 'password'
+    }
+  }
+
+
+  // when user submit this method call 
+
+  const submit = (e) => {
+
+    e.preventDefault()
+
+    // uservalue 
+    const user = {
+      username: username,
+      password: password
+    }
+    console.log(user)
+
+    // Create the POST requuest
+    
+
+}
+
+
+const MyStyle = {
+  borderRadius: '15px',
+  boxShadow: '5px 12px 8px gray'
+}
+return (
+  <div className='container my-5 '>
+    <div style={MyStyle} className='container my-5 p-5 bg-light w-50' >
+
+      <form onSubmit={submit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" className="form-control" id="username" required onChange={event => setUsername(event.target.value)} />
+
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" required onChange={evenet => setPassword(evenet.target.value)} />
+        </div>
+
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="showpass" onClick={ShowPass} />
+          <label className="form-check-label" htmlFor="showpass">Show password</label>
+        </div>
+        <button type="submit" className="btn btn-primary" >Login</button>
+      </form>
+
+    </div >
+
+
+
+  </div>
+)
 }
--- Frontend/front-end/src/Components/Navbar.js
+++ Frontend/front-end/src/Components/Navbar.js
@@ -1,40 +1,30 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
 
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <Link to="/" className="navbar-brand" ><i className="bi bi-broadcast-pin"></i></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
+            
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <Link to="/" className="nav-link" >Home <span className="sr-only">(current)</span></Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
+  
+            
           </ul>
           <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <Link to="/login" className="nav-link" >Login</Link>   <Link to="/logout" className="nav-link" >LogOut</Link>
+
+            
           </form>
         </div>
       </nav>
